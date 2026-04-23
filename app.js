0600cdf24074d8983f3b48a284b62fe05e40cae8
MDalmi <maicondalmina.pf003@academico.ifsul.edu.br>
2026-04-22 22:05:56 -0300

    fix: update .gitignore

--- app.js
+++ app.js
@@ -3,27 +3,26 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocs = require('./config/swagger');
 const rotas = require('./routes/index');
 const errorHandler = require('./middlewares/erroHandler');
-const sequelize = require('./config/database'); // Importa a conexão com o Neon
+const sequelize = require('./config/database');
 
-// 1. Inicializa o app PRIMEIRO
+// 1. Inicializa o app 
 const app = express();
 const PORT = 3000;
 
-// 2. Middlewares (A ordem importa: JSON sempre antes das rotas)
+// 2. Middlewares 
 app.use(express.json());
 
 // 3. Rotas da API e Documentação
 app.use('/api', rotas);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
-// 4. Tratamento de Erros (Sempre por último nas configurações do express)
+// 4. Tratamento de Erros 
 app.use(errorHandler);
 
-// 5. Sincroniza o banco e DEPOIS liga o servidor
 sequelize.sync().then(() => {
     console.log('Tabelas sincronizadas no banco Neon com sucesso!');
     
-    // O app.listen fica APENAS AQUI DENTRO
+    
     app.listen(PORT, () => {
         console.log(`Servidor rodando em http://localhost:${PORT}`);
         console.log(`Swagger disponível em http://localhost:${PORT}/api-docs`);
